7f4eb0ba69b1b6f3ac34a8ecb845d9f4ca650cae
seerosecode <keshalakemusic@gmail.com>
2019-05-21 14:00:43 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+passenger1 = Passenger.create(name: "Benjamin")
+passenger2 = Passenger.create(name: "Rose")
+passenger3 = Passenger.create(name: "Melanie")
+
+taxi1 = Taxi.create(name: "yellow cab 1")
+taxi2 = Taxi.create(name: "yellow cab 2")
+taxi3 = Taxi.create(name: "yellow cab 3")
+
+ride1 = Ride.create(passenger_id: passenger1.id, taxi_id: taxi1.id)
+ride2 = Ride.create(passenger_id: passenger1.id, taxi_id: taxi2.id)
+ride3 = Ride.create(passenger_id: passenger1.id, taxi_id: taxi3.id)
+ride4 = Ride.create(passenger_id: passenger2.id, taxi_id: taxi1.id)
+ride5 = Ride.create(passenger_id: passenger2.id, taxi_id: taxi2.id)
+ride6 = Ride.create(passenger_id: passenger2.id, taxi_id: taxi3.id)
+ride7 = Ride.create(passenger_id: passenger3.id, taxi_id: taxi1.id)
+ride8 = Ride.create(passenger_id: passenger3.id, taxi_id: taxi2.id)
+ride9 = Ride.create(passenger_id: passenger3.id, taxi_id: taxi3.id)
